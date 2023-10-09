76df42bbe3b3d2cb97ffcfe30d95519ca986e084
caidd.fun.cjy@qq.com <caidd.fun.cjy@qq.com>
2023-10-09 20:13:19 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(appleNum: i32) -> i32 {
+    if appleNum <= 40 {
+        return appleNum * 2;
+    }
+    appleNum
+}
 
 // Don't modify this function!
 #[test]
